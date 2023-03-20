9a8339a9c35e18ef59147465f0eec99b1b3de734
Sherif Abdalla <sherif@abdalla.uk>
2023-03-20 00:10:36 +0000

    Set release version of nixos

--- flake.nix
+++ flake.nix
@@ -1,14 +1,23 @@
 {
-  description = "golang development environment (neovim + nvim_lsp + treesitter)";
+  description = "A Nix-flake-based Nix development environment";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        {
-          devShell = import ./shell.nix { inherit pkgs; };
-        }
-      );
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+       pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShell = import ./shell.nix { inherit pkgs; };
+      }
+    );
 }
+
